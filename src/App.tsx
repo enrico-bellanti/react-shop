7dd8a487faa9944c765c5f87b35f41fe2dff4400
ebellanti <enrico.bellanti@nais-solutions.it>
2024-09-30 17:38:50 +0200

    created set button classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@
 function App() {
   return (
     <div className="page">
-      <h1 className="title">DEMO</h1>
-        Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.
+      <h1 className="title">Shop</h1>
+      <button className="btn">Button</button>
+      <button className="btn" disabled>Button</button>
+      <button className="btn dark">Dark</button>
+      <button className="btn danger">Danger</button>
+      <button className="btn primary">Primary</button>
+      <button className="btn accent">Accent</button>
+      <button className="btn success outline">Success</button>
+      <button className="btn success outline">Outline</button>
+      <button className="btn primary">Base</button>
+      <button className="btn primary lg">Large</button>
     </div>
   )
 }
