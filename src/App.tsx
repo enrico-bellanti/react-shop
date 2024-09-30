d186bf01e7a2fd25b9ea3aab15082f7956cf062e
ebellanti <enrico.bellanti@nais-solutions.it>
2024-09-30 17:50:09 +0200

    added table custom design

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="page">
       <input type="text" />
       <input type="text" className="error" />
-      <h1 className="title">Shop</h1>
+      <h1 className="title">UIKIT</h1>
       <button className="btn">Button</button>
       <button className="btn" disabled>Button</button>
       <button className="btn dark">Dark</button>
@@ -16,6 +16,32 @@ function App() {
       <button className="btn success outline">Outline</button>
       <button className="btn primary">Base</button>
       <button className="btn primary lg">Large</button>
+      <table className="table-auto w-full hover">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+          <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+          </tr>
+          <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
