50b00d4d2459bbd9747160c9c4f58b15071ed63d
ebellanti <enrico.bellanti@nais-solutions.it>
2024-10-02 15:18:43 +0200

    implemented checkout page and custom hook useCheckout

--- src/pages/checkout/CheckoutPage.tsx
+++ src/pages/checkout/CheckoutPage.tsx
@@ -1,8 +1,47 @@
+import clsx from 'clsx';
+import { useCheckout } from './hooks/useCheckout';
+
 export function CheckoutPage(){
+    const {
+        validators, actions,
+        user, dirty, totalCartCost
+      } = useCheckout();
+
     return (
-        <div>
+        <div className="max-w-sm mx-auto">
             <h1 className="title">Checkout</h1>
-            content
+            
+            <div className="text-xl my-3 border-b">$ {totalCartCost}</div>
+
+            <form className="flex flex-col gap-3" onSubmit={actions.sendOrder}>
+                Your name:
+                <input 
+                    type="text" 
+                    placeholder="your name" 
+                    value={user.name}
+                    name="name"
+                    onChange={actions.changeHandler}
+                    className={clsx({error: !validators.isNameValid && dirty})}
+                />
+
+                Your email:
+                <input 
+                    type="email" 
+                    placeholder="your email" 
+                    value={user.email}
+                    name="email"
+                    className={clsx({error: !validators.isEmailValid && dirty})}
+                    onChange={actions.changeHandler}
+                />
+
+                <button 
+                    type="submit" 
+                    className={clsx('btn', { primary: !validators.isValid, success: validators.isValid})}
+                    disabled={!validators.isValid}
+                >
+                    Confirm Order
+                </button>
+            </form>
         </div>
     )
 }
