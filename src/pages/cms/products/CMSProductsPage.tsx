a0c51f5ce2640f47bdfe73eb4cae3112b214f102
ebellanti <enrico.bellanti@nais-solutions.it>
2024-10-03 13:10:25 +0200

    implemented product state action reducer api-service hook

--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,8 +1,22 @@
+import { useProductsService } from "../../../services/products"
+
 export function CMSProductsPage() {
+    const {state, actions} = useProductsService()
+
     return (
         <div>
-            <h1 className="title">CMS products</h1>
-            content
+        <h1 className="title">CMS</h1>
+
+        Pagina Prodotti
+
+        <hr className="my-8"/>
+
+        {state.pending && <div>loading...</div>}
+        {state.error && <div>{state.error}</div>}
+
+        <button className="btn primary" onClick={actions.getProducts}>GET</button>
+
+        <pre>{JSON.stringify(state, null, 2)}</pre>
         </div>
     )
 }
---
+++ src/services/products/useProductsService.ts
@@ -0,0 +1,71 @@
+import { useReducer } from "react";
+import { Product } from "../../model/product";
+import * as ProductsApi from "./products.api";
+import { initialState, productsReducer } from "./products.reducer";
+
+
+export function useProductsService() {
+    const [state, dispatch] = useReducer(productsReducer, initialState);
+
+    async function getProducts() {
+        dispatch({ type: 'pending', payload: true });
+        try {
+            const res = await ProductsApi.get();
+            dispatch({ type: 'productsGetSuccess', payload: res.items })
+        } catch (e) {
+            dispatch({ type: 'error', payload: 'Products not loaded' })
+        }
+    }
+
+    async function deleteProduct(id: string) {
+        dispatch({ type: 'pending', payload: true })
+        try {
+            await ProductsApi.remove(id);
+            dispatch({ type: 'productDeleteSuccess', payload: id })
+        } catch (e) {
+            dispatch({ type: 'error', payload: 'Products not deleted' })
+        }
+    }
+
+
+
+    async function addProduct(product: Partial<Product>) {
+        dispatch({ type: 'pending', payload: true })
+        try {
+            const res = await ProductsApi.add(product);
+            dispatch({ type: 'productAddSuccess', payload: res })
+        } catch (e) {
+            dispatch({ type: 'error', payload: 'Products not added' })
+        }
+    }
+
+    async function editProduct(product: Partial<Product>) {
+        dispatch({ type: 'pending', payload: true })
+        try {
+            const res = await ProductsApi.edit(product);
+            dispatch({ type: 'productEditSuccess', payload: res })
+        } catch (e) {
+            dispatch({ type: 'error', payload: 'Products not edited' })
+        }
+    }
+
+    function setActiveItem(product: Product | {}) {
+        dispatch({ type: 'productSetActive', payload: product })
+    }
+
+    function resetActiveItem() {
+        dispatch({ type: 'productSetActive', payload: null })
+    }
+
+    return {
+        actions: {
+            getProducts,
+            deleteProduct,
+            addProduct,
+            editProduct,
+            setActiveItem,
+            resetActiveItem
+        },
+        state
+    }
+}
